74f1a297caac64186152ee06706ba4a1f5076f04
Elias <eliasdesouza@biosistemico.com.br>
2021-11-26 13:27:42 -0300

    :rocket: configurando o projeto e adiciona funcionalidade de workers

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,12 @@
-import * as React from 'react';
+import React from 'react';
+import { View } from 'react-native';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-queuely';
+// import { Container } from './styles';
 
-export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
+const Application: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+   <View></View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
+export default Application;
